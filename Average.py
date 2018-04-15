3c27b27ecd6caed639430af3034467dec99d1f3b
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-04-15 18:53:41 +0100

    Create Average.py

---
+++ Average.py
@@ -0,0 +1,12 @@
+# Declan Reidy - Project initiation
+
+# https://www.geeksforgeeks.org/find-average-list-python/
+
+def AverageDR(dataset):
+
+  return sum(dataset)/len(dataset)
+
+dataset = [2,3,4,5,6]
+Average = AverageDR(dataset)
+  
+print("The average of this dataset is:", Average)
